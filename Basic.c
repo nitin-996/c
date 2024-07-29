efdbc93bb7d06dfc437b3068ef9801188ae47a97
torrento <sachinles99@gmail.com>
2024-07-29 16:02:22 +0530

    added

---
+++ Basic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("hello world \n");
+    printf("hello \n " );
+    
+
+    int age;
+    scanf("%d" , &age);
+    printf("age is %d", age);
+
+    int a, b;
+    a=5;
+    b=2;
+    printf("%f",pow(a,b));
+}
